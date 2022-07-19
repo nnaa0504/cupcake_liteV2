b8e889dfe53e6520f346223a96c2ff29d27fc88c
Naso <43310384+nnaa0504@users.noreply.github.com>
2022-07-19 12:31:17 +0900

    Update config.h

--- cupcakelite/config.h
+++ cupcakelite/config.h
@@ -25,7 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    naso @mk_nnaa
 #define PRODUCT         CupCake Lite
-#define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 1
